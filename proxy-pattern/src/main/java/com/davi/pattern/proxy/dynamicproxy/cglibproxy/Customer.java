d0ed4dadf0272a132638ab894af4ff6a15736a03
ChildHe <hedawei0816@126.com>
2021-05-27 00:18:05 +0800

    commit proxy pattern

---
+++ proxy-pattern/src/main/java/com/davi/pattern/proxy/dynamicproxy/cglibproxy/Customer.java
@@ -0,0 +1,12 @@
+package com.davi.pattern.proxy.dynamicproxy.cglibproxy;
+
+/**
+ * @Date 2021/5/25 23:48
+ * @Created by hdw
+ */
+public class Customer {
+
+    public void findLove() {
+        System.out.println("儿子要求：肤白貌美大长腿！");
+    }
+}
