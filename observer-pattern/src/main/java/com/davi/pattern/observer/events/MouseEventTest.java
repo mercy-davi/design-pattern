5b66c8141c0e3fe85c4b61da85aa7f8dc599a424
ChildHe <hedawei0816@126.com>
2021-06-05 14:21:12 +0800

    commit observer pattern

---
+++ observer-pattern/src/main/java/com/davi/pattern/observer/events/MouseEventTest.java
@@ -0,0 +1,29 @@
+package com.davi.pattern.observer.events;
+
+
+import com.davi.pattern.observer.events.mouseevent.Mouse;
+import com.davi.pattern.observer.events.mouseevent.MouseEventCallback;
+import com.davi.pattern.observer.events.mouseevent.MouseEventType;
+
+/**
+ * @Date 2021/6/5
+ * @Created by hdw
+ */
+public class MouseEventTest {
+    public static void main(String[] args) {
+
+        MouseEventCallback callback = new MouseEventCallback();
+
+        Mouse mouse = new Mouse();
+
+        //@谁？  @回调方法
+        mouse.addListener(MouseEventType.ON_CLICK, callback);
+        mouse.addListener(MouseEventType.ON_FOCUS, callback);
+
+        mouse.click();
+
+        mouse.focus();
+
+
+    }
+}
---
+++ observer-pattern/src/main/java/com/davi/pattern/observer/events/mouseevent/Keyboard.java
@@ -0,0 +1,19 @@
+package com.davi.pattern.observer.events.mouseevent;
+
+import com.davi.pattern.observer.events.core.EventListener;
+
+/**
+ * @Date 2021/6/5
+ * @Created by hdw
+ */
+public class Keyboard extends EventListener {
+
+    public void down() {
+
+    }
+
+    public void up() {
+
+    }
+
+}
